c2e7b258d5d3b4250ade244b6439949aa219ef58
VovaSles <vovaswebdev@gmail.com>
2021-01-30 15:51:36 +0200

    Updates

---
+++ precache-manifest.51791f4fbe9a47306d2303dddb1c6ccc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181c46b63bd1fb3d321a98dd03b2ac69",
+    "url": "/MyPlacesfrontend/index.html"
+  },
+  {
+    "revision": "f2c3b91be1d2f3f5478b",
+    "url": "/MyPlacesfrontend/static/css/0.0fdfc447.chunk.css"
+  },
+  {
+    "revision": "39b4ddf74f336b0df3d9",
+    "url": "/MyPlacesfrontend/static/css/4.3d7c0d71.chunk.css"
+  },
+  {
+    "revision": "0b843e075a7f1fac7822",
+    "url": "/MyPlacesfrontend/static/css/5.9628b1da.chunk.css"
+  },
+  {
+    "revision": "86f720aa64e71de21c11",
+    "url": "/MyPlacesfrontend/static/css/6.284b6b37.chunk.css"
+  },
+  {
+    "revision": "bf71eb1a585ff94769d7",
+    "url": "/MyPlacesfrontend/static/css/7.02e69444.chunk.css"
+  },
+  {
+    "revision": "2a8ef1c8e2bbaed9ed46",
+    "url": "/MyPlacesfrontend/static/css/8.b55ccdaa.chunk.css"
+  },
+  {
+    "revision": "b519f90c5846c1ad8bba",
+    "url": "/MyPlacesfrontend/static/css/main.54653e49.chunk.css"
+  },
+  {
+    "revision": "f2c3b91be1d2f3f5478b",
+    "url": "/MyPlacesfrontend/static/js/0.92ceab5b.chunk.js"
+  },
+  {
+    "revision": "39343acedfd490893737",
+    "url": "/MyPlacesfrontend/static/js/3.1916cbe2.chunk.js"
+  },
+  {
+    "revision": "39b4ddf74f336b0df3d9",
+    "url": "/MyPlacesfrontend/static/js/4.374cba1c.chunk.js"
+  },
+  {
+    "revision": "0b843e075a7f1fac7822",
+    "url": "/MyPlacesfrontend/static/js/5.3fc7c838.chunk.js"
+  },
+  {
+    "revision": "86f720aa64e71de21c11",
+    "url": "/MyPlacesfrontend/static/js/6.f872a783.chunk.js"
+  },
+  {
+    "revision": "bf71eb1a585ff94769d7",
+    "url": "/MyPlacesfrontend/static/js/7.6151445a.chunk.js"
+  },
+  {
+    "revision": "2a8ef1c8e2bbaed9ed46",
+    "url": "/MyPlacesfrontend/static/js/8.273936f8.chunk.js"
+  },
+  {
+    "revision": "b519f90c5846c1ad8bba",
+    "url": "/MyPlacesfrontend/static/js/main.b3ba9fc6.chunk.js"
+  },
+  {
+    "revision": "6c33dcfe0f876dd09603",
+    "url": "/MyPlacesfrontend/static/js/runtime-main.44461e93.js"
+  }
+]);
